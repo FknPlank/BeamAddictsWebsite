42c0a6b866f3f9954b1b21e0b33678e084253512
StayClaxxy <demboyzxreverse@gmail.com>
2024-12-21 22:20:49 -0600

    accordion maybe?

--- app/Whitelist/page.tsx
+++ app/Whitelist/page.tsx
@@ -1,25 +1,79 @@
+"use client";
+
+import { Accordion, AccordionItem } from "@nextui-org/react";
+import { faDiscord } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import { title } from "@/components/primitives";
-import { Smokum } from "next/font/google";
 
 export default function WhitelistPage() {
   return (
-    <>
-      <div>
+    <div>
+      <div className="flex flex-row mb-5">
         <h1 className={title()}>Get Whitelisted Today!</h1>
       </div>
-      <br />
-  <div className="Box-Shadow">
-    <h1>Whitelist Requirements:</h1>
-    <ul className="">
-      <li className="text-left">1). You must be 18+ years old (Certain situations we will waive this requirement).</li>
-      <li className="text-left">2). Have some working knowledge of how a demolition derby works or at least be willing to follow directions.</li>
-      <li className="text-left">3). Must have paid for RDP Patreon (Screenshot Required).</li>
-      <li className="text-left">4). We ask that during events that you be in one of the voice channels for proper communication.</li>
-      <li className="text-left">
-        5). This server is personally funded and developed by me. I'm open to any suggestions. Please put them in <span>#suggestions</span>.
-      </li>
-    </ul>
-</div>
-    </>
+      <div className="flex flex-row">
+        <Accordion defaultExpandedKeys={["1"]} variant="splitted">
+          <AccordionItem
+            key="1"
+            aria-label="What are the rules?"
+            className="mb-2"
+            title="What are the rules?"
+          >
+            <ul className="list-disc list-inside text-left">
+              <li className="mb-2">
+                You must be 18+ years old (In certain situations we will waive
+                this requirement)
+              </li>
+              <li className="mb-2">
+                Have some working knowledge of how a demolition derby works or
+                at least be willing to follow directions
+              </li>
+              <li className="mb-2">
+                Must have paid for RDP Patreon (Screenshot Required)
+              </li>
+              <li className="mb-2">
+                We ask that during events that you be in one of the voice
+                channels for proper communication.
+              </li>
+              <li className="mb-2">
+                This server is personally funded and developed by me. I&apos;m
+                open to any suggestions. Please put them in the suggestions
+              </li>
+            </ul>
+          </AccordionItem>
+          <AccordionItem
+            key="2"
+            aria-label="How do I get whitelisted?"
+            className="mb-2"
+            title="How do I get whitelisted?"
+          >
+            Join the discord to get more info on whitelisting!
+            <div className="flex flex-row my-4 justify-center">
+              <a
+                href="https://discord.gg/WfJywRExFm"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <FontAwesomeIcon
+                  className="text-blue-500"
+                  icon={faDiscord}
+                  size="4x"
+                />
+              </a>
+            </div>
+          </AccordionItem>
+          <AccordionItem
+            key="3"
+            aria-label="Can I add my custom skins?"
+            className="mb-2"
+            title="Can I add my custom skins?"
+          >
+            Yes! You can add pre-made skins, or commission our staff to make one
+            for you!.
+          </AccordionItem>
+        </Accordion>
+      </div>
+    </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import "@/styles/globals.css";
 import { Metadata, Viewport } from "next";
-import { Link } from "@nextui-org/link";
 import clsx from "clsx";
 
 import { Providers } from "./providers";
@@ -34,7 +33,6 @@ export default function RootLayout({
 }) {
   return (
     <html suppressHydrationWarning lang="en">
-      <head />
       <body
         className={clsx(
           "min-h-screen bg-background font-sans antialiased",
@@ -47,9 +45,7 @@ export default function RootLayout({
             <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
               {children}
             </main>
-            <footer className="w-full flex items-center justify-center py-3">
-              <h2>This is the footer</h2>
-            </footer>
+            <footer className="w-full flex items-center justify-center py-3" />
           </div>
         </Providers>
       </body>
